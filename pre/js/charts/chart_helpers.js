6441567da35eed1ee8ee0755c9c22e506b64a573
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-03-11 12:45:20 +0100

    Cambio en iframe

--- pre/js/charts/chart_helpers.js
+++ pre/js/charts/chart_helpers.js
@@ -5,5 +5,5 @@ export function setFixedIframeUrl(chart, title) {
     
     //Iframe responsive
     let id2 = document.getElementById('iframe-responsive');
-    id2.innerHTML = `<div id="viz_cchs_informe_perfil_mayores_2022"></div><script type="text/javascript" src="https://pym.nprapps.org/pym.v1.min.js"></script><script>let pymParent = new pym.Parent('viz_cchs_informe_perfil_mayores_2022', 'https://EnvejecimientoEnRed.github.io/informe-2022-viz/?chart=${title}&iframe=responsive', {});</script>`;
+    id2.innerHTML = `<div id="viz_cchs_informe_perfil_mayores_2022"></div><script type="text/javascript" src="https://pym.nprapps.org/pym.v1.min.js"></script><script>let pymParent = new pym.Parent('viz_cchs_informe_perfil_mayores_2022', 'https://EnvejecimientoEnRed.github.io/${chart}/?iframe=responsive', {});</script>`;
 }
